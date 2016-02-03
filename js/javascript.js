b7aec33a733d40d532781321747300309429e2ab
juankboards <juankboards@hotmail.com>
2016-02-02 22:56:15 -0500

    add style[C

--- js/javascript.js
+++ js/javascript.js
@@ -2,13 +2,15 @@ function printTime () {
 	
 	if (workRunning){
 		counterRunning(workDuration);
+		$("body").css("background-image","url(https://s1.yimg.com/bt/api/res/1.2/eleQVdMo5McWXj8YR5IJUg--/YXBwaWQ9eW5ld3M7cT04NTt3PTYzMA--/http://l.yimg.com/os/publish-images/omg/2015-05-26/b2a0df80-03cb-11e5-a8a1-6dc8d3245017_shia-labeouf-do-it.gif)");
 		if (diff<=0) {
 			workRunning = false;
 			breakRunning = true;
 			counterStart();
 		};		
-	} else {
+	} else if (breakRunning) {
 		counterRunning(breakDuration);
+		$("body").css("background-image","url(https://media.giphy.com/media/eYbS7lujv4Ig0/giphy.gif)");
 		if (diff<=0) {
 			workRunning = true;
 			breakRunning = false;
@@ -16,21 +18,20 @@ function printTime () {
 			sessions--;
 			if (sessions<1) {
 				clearInterval(intervalId);
-				$("#tittle").text("Great Job!");
-				$("#counterButtons").css("display","block");
-				return;
+				$("#tittle").text("Great Job!");	
+				$("body").css("background-image","url(http://i.imgur.com/NY5OPT1.gif?noredirect)");							
 			}
-		};			
-	
-	
+		};	
+	} else {
+		$("#tittle").text("Take your time and start when you feel good");	
+	}
 
-	} 
 }
 
-function counterStart (){	
-	start = Date.now(),
+function counterStart (){		
+	start = Date.now();
 	diff = 0;
-	seconds = 0
+	seconds = 0;
 	minutes = 0;	
 	time = "";
 }
@@ -47,28 +48,73 @@ function counterRunning (duration) {
 	time = minutes + ":" + seconds;
 
 	if (diff>=0){
-		$("#tittle").text(time);
+		$("#secondTittle").text(time);
 	}
+}
+
+function startPause (){		
+	workRunning = false;
+	breakRunning = false;
+	$("#pause").text("Start timer");
 }
 
 $(document).ready(function(){	
 	$("#start").on({
 		'click': function (){
-			workRunning = true,
-			breakRunning = false
+			workRunning = true;
+			breakRunning = false;
 			sessions = parseInt($("#sessions").val());
 			breakDuration = 60 * parseInt($("#break").val());
 			workDuration = 60 * parseInt($("#work").val());
 			
 			
-			if (sessions>0 && workDuration>0 && breakDuration>0){
-				$(".timeSet").css("display","none");
+			if (sessions>0 && workDuration >0 && breakDuration>0){
+				$("#secondTittle").toggle();
+				$("#timeSet").toggle();		
+				$("#counterButtons").toggle();
+
 				counterStart();			
 				printTime();
-				intervalId = setInterval(printTime,1000);	
-			}
-				
+				intervalId = setInterval(printTime,200);	
+			}				
 		}
-	})	
-	
+	});	
+
+	$("#pause").on({				
+		'click': function(){
+			lastWork = workRunning;
+			lastBreak = breakRunning;
+			$("#pause").toggle();
+			$("#startAgain").toggle();
+			if (workRunning){
+				workDuration = diff;
+				workRunning = false;
+			} else {
+				breakDuration = diff;
+				breakRunning = false;
+			}						
+		}
+	});
+
+	$("#startAgain").on({		
+		'click': function(){
+			$("#pause").toggle();
+			$("#startAgain").toggle();
+			workRunning = lastWork;
+			breakRunning = lastBreak;
+			counterStart();
+		}
+	});
+
+	$("#setTime").on({		
+		'click': function(){
+			$("body").css("background-image","url(https://media.giphy.com/media/wErJXg1tIgHXG/giphy.gif)");
+			$("#timeSet").toggle();
+			$("#counterButtons").toggle();
+			$("#secondTittle").text("You can do it!");	
+			clearInterval(intervalId);
+		}
+	});
+
+
 });
